26c89058ba5356250b9ae981b84b22aa884d9ae2
dinhs-frontend <haisang7@live.nl>
2020-05-19 15:59:07 +0200

    week 3

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 
 var express = require('express')
+const ejs = require('ejs')
 
 express()
     .use(express.static('static'))
-    .get('/', movies)
+    .set ('view engine', 'ejs')
+    .set ('views', 'view')
+    .get('/', home)
     .get('/about', about)
+    .get ('/404',notFound)
+    
+    .get ("/", (req,res) => resend(""))
     .listen(8000)
 
-    function movies (req, res) {
+    function home (req, res) {
         res.status(200).send('server says hello')
     }
 
     function about (req, res) {
         res.status(200).send('about page')
     }
+    function notFound (req, res) {
+        res.status(404).render('not-found.ejs')
+    }
+
+
+
+
+
