c29a8dcde4a733ef140914a7cd826f4a3e16157f
dinhs-frontend <haisang7@live.nl>
2020-05-25 01:01:32 +0200

    new ejs + index geupdate

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
+const express = require('express');
+const app = express();
+const port = 2000;
 
-var express = require('express')
-const ejs = require('ejs')
+app.listen(port);
+app.use(express.static('static')) 
+app.set ('view engine', 'ejs')     
+app.set('views', 'view')   
 
-express()
-    .use(express.static('static'))
-    .set ('view engine', 'ejs')
-    .set ('views', 'view')
-    .get('/', home)
-    .get('/about', about)
-    .get ('/404',notFound)
-    
-    .get ("/", (req,res) => resend(""))
-    .listen(8000)
+// app.get ('/', (req, res) => res.send ('hello world'));
+app.get ('/', (req, res) => res.render ('index.ejs'));
 
-    function home (req, res) {
-        res.status(200).send('server says hello')
-    }
+app.get ('/not-found', (req, res, next) => {
+    res.render('not-found.ejs', {
+        title: 'Not found!',
+        showTitle: false, 
+        description: 'Yo bro het werkt niet man SORRY!',
+     });
+});
 
-    function about (req, res) {
-        res.status(200).send('about page')
-    }
-    function notFound (req, res) {
-        res.status(404).render('not-found.ejs')
-    }
+app.get ('/topics', (req, res, next) => {
+    res.render('topics.ejs', {
+        title: 'test', 
+        showTitle: false,
+        data: [ 'pasta', 'pho', 'pizza', 'patat']
+    });
+});
 
-
-
+app.use(function(req, res, next) {
+    res.status(404).render('404.ejs');
+});
 
 
+
+    // .use(function (req, res, next) {
+    //     res
+    //         .status(404)
+    //         .send('That does not exist');
+    // });
+
+// const express = require('express') 
+// const ejs = require('ejs') express()
+// .use(express.static('static'))     
+// .set ('view engine', 'ejs')     
+// .set('views', 'view')     
+// .get('/', home)     
+// .get('/about', about)      
+// .get('/404',notFound) 
+// .get ("/", (req,res) => resend("")) .listen(8000) 
+
+// function home (req, res) {     res.status(200).send('server says hello') } 
+// function about (req, res) {     res.status(200).send('about page') } 
+// function notFound(req, res) {     res.status(404).render('not-found.ejs') }
